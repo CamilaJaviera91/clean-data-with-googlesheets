15628f06ca4cd0d3fecc5f94ba46042eee3bc907
Camila Muñoz <cjmunozn@hotmail.com>
2024-11-23 18:18:55 -0300

    feat(csv-to-sheets): add normal lib, google_sheets_utils lib, warnings and plt style

---
+++ csv_to_sheets.py
@@ -0,0 +1,15 @@
+#Add libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+from matplotlib.backends.backend_pdf import PdfPages
+import math
+
+#Add libraries from google_sheets_utils
+from google_sheets_utils import sheets_to_dataframe as sd
+from google_sheets_utils import csv_to_sheets as cs
+
+#Add warnings and plt style
+warnings.filterwarnings('ignore')
+plt.style.use("default")
