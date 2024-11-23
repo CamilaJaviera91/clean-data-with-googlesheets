fc388463d9d23f6395dbfbcfbc4671476e7117ed
Camila Muñoz <cjmunozn@hotmail.com>
2024-11-22 23:36:17 -0300

    add the current directory and the id of the spreadsheet that is going to be used

--- google_sheets_utils.py
+++ google_sheets_utils.py
@@ -9,3 +9,10 @@ from googleapiclient.errors import HttpError
 #Add warnings and plt style
 warnings.filterwarnings('ignore')
 plt.style.use("default")
+
+#current directory
+FOLDER = './'
+
+#Spreadsheet ID (you can find it in the URL of your Google Sheet example: https://docs.google.com/spreadsheets/d/1aBcD1234EfGhI56789JKLMnOpQrStUvWxYz/edit#gid=0)
+#"1aBcD1234EfGhI56789JKLMnOpQrStUvWxYz" this is the id that we need
+SPREADSHEET_ID = '1aBcD1234EfGhI56789JKLMnOpQrStUvWxYz'
