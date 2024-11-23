8e64935ba0e24d7c79f8a93163557ab1f8b3db18
Camila Muñoz <cjmunozn@hotmail.com>
2024-11-22 23:27:37 -0300

    feat(clean data): add warnings and plt style

--- google_sheets_utils.py
+++ google_sheets_utils.py
@@ -1,6 +1,11 @@
+#Add libraries
 import pandas as pd
 import matplotlib.pyplot as plt
 import warnings
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+
+#Add warnings and plt style
+warnings.filterwarnings('ignore')
+plt.style.use("default")
